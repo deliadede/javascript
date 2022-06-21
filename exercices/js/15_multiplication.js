3a21d9b3dfb67b9c627143db57aee0b92b8e02c5
Delia DANCIU <delia.danciu@lepoles.org>
2022-06-21 16:49:56 +0200

    javascript

---
+++ exercices/js/15_multiplication.js
@@ -0,0 +1,8 @@
+// alert("hy");
+
+var choix =prompt("Choisis un nombre");
+
+for(var i=1; i<= 10; i++){
+    var resultat = choix * i;
+    document.write(choix +"x" + i + " = " + resultat + "<br>")
+}
